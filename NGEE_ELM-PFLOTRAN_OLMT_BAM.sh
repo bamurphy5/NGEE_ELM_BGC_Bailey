6dd6c0ef9b199e704b580507b29047d155f52d7a
Bailey Murphy <42819353+bamurphy5@users.noreply.github.com>
2024-11-13 11:02:16 -0500

    Update NGEE_ELM-PFLOTRAN_OLMT_BAM.sh

--- NGEE_ELM-PFLOTRAN_OLMT_BAM.sh
+++ NGEE_ELM-PFLOTRAN_OLMT_BAM.sh
@@ -71,8 +71,8 @@ varlist="TOTVEGC,TOTSOMC,TOTLITC,SOIL1C_vr,SOIL2C_vr,SOIL3C_vr,SOIL4C_vr,LITR1C_
 soil_O2,HR,GPP,NEE,NPP,SMINN,SMINN_TO_PLANT,DIC_vr,SIC_vr,H2OSOI,H2OSFC,SOILLIQ,SOILICE,ZWT,\
 QFLX_EVAP_TOT,QVEGT,watsat,chem_dt,soil_salinity,soil_pH,DOC_vr,DIC_vr,DOC_RUNOFF,DIC_RUNOFF,SMIN_NO3_RUNOFF,\
 soil_sulfate,soil_sulfide,CH4_vr,CH4FLUX_ALQUIMIA,QDRAI,TSOI,soil_Fe2,soil_FeOxide,soil_FeS,soil_acetate"
-python site_fullrun.py --site AK-BEO --sitegroup NGEEArctic --caseidprefix Alaska_alquimia_test2  \
---noad --tstep 1 --nyears_final_spinup 15 --nyears_transient 15 \
+python site_fullrun.py --site AK-BEO --sitegroup NGEEArctic --caseidprefix Alaska_alquimia_test_AD  \
+--nyears_ad_spinup 15 --tstep 1 --nyears_final_spinup 15 --nyears_transient 15 \
 --cpl_bypass --machine cades --no_dynroot --gswp3 --nofire --nopftdyn --nopointdata \
 --model_root /home/9v6/ELM-alquimia/E3SM --ccsm_input /nfs/data/ccsi/proj-shared/E3SM/pt-e3sm-inputdata \
 --domainfile $domain \
