62b48d096e1f625b7c7eca1224c595c5f6f2d6f0
Bailey Murphy <42819353+bamurphy5@users.noreply.github.com>
2024-11-14 15:05:02 -0500

    Update NGEE_ELM-PFLOTRAN_OLMT_BAM.sh
    added H2OSFC_TIDE to output variables

--- NGEE_ELM-PFLOTRAN_OLMT_BAM.sh
+++ NGEE_ELM-PFLOTRAN_OLMT_BAM.sh
@@ -95,7 +95,7 @@ surf=/home/9v6/NGEE_ELM/BEO_surfdata_multicell.nc
 varlist="TOTVEGC,TOTSOMC,TOTLITC,SOIL1C_vr,SOIL2C_vr,SOIL3C_vr,SOIL4C_vr,LITR1C_vr,LITR2C_vr,LITR3C_vr,LEAFC,\
 soil_O2,HR,GPP,NEE,NPP,SMINN,SMINN_TO_PLANT,DIC_vr,SIC_vr,H2OSOI,H2OSFC,SOILLIQ,SOILICE,ZWT,\
 QFLX_EVAP_TOT,QVEGT,watsat,chem_dt,soil_salinity,soil_pH,DOC_vr,DIC_vr,DOC_RUNOFF,DIC_RUNOFF,SMIN_NO3_RUNOFF,\
-soil_sulfate,soil_sulfide,CH4_vr,CH4FLUX_ALQUIMIA,QDRAI,TSOI,soil_Fe2,soil_FeOxide,soil_FeS,soil_acetate"
+soil_sulfate,soil_sulfide,CH4_vr,CH4FLUX_ALQUIMIA,QDRAI,TSOI,soil_Fe2,soil_FeOxide,soil_FeS,soil_acetate, H2OSFC_TIDE"
 python site_fullrun.py --site AK-BEO --sitegroup NGEEArctic --caseidprefix Alaska_alquimia_test_BAM  \
 --nyears_ad_spinup 15 --tstep 1 --nyears_final_spinup 15 --nyears_transient 15 \
 --cpl_bypass --machine cades --no_dynroot --gswp3 --nofire --nopftdyn --nopointdata \
