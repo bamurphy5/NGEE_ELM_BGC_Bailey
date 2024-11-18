7b9e75b85669f484d46f1729c24f46e35dd84d8e
Bailey Murphy <42819353+bamurphy5@users.noreply.github.com>
2024-11-18 15:28:02 -0500

    Update NGEE_ELM-PFLOTRAN_OLMT_BAM.sh
    Updated to also output ALT and H2OSFC_TIDE

--- NGEE_ELM-PFLOTRAN_OLMT_BAM.sh
+++ NGEE_ELM-PFLOTRAN_OLMT_BAM.sh
@@ -87,7 +87,7 @@ python site_fullrun.py --site AK-BEO --sitegroup NGEEArctic --caseidprefix Alask
 
 
 
-#AD spinup and tidal forcing, but using my updated surface files 
+#AD spinup and tidal forcing, but using my updated surface files. Updated to also output ALT and H2OSFC_TIDE
 site=beo
 metdir=/nfs/data/ccsi/proj-shared/E3SM/pt-e3sm-inputdata/atm/datm7/GSWP3_daymet/cpl_bypass_$site
 domain=/home/9v6/NGEE_ELM/BEO_domain_multicell.nc
@@ -95,7 +95,7 @@ surf=/home/9v6/NGEE_ELM/BEO_surfdata_multicell.nc
 varlist="TOTVEGC,TOTSOMC,TOTLITC,SOIL1C_vr,SOIL2C_vr,SOIL3C_vr,SOIL4C_vr,LITR1C_vr,LITR2C_vr,LITR3C_vr,LEAFC,\
 soil_O2,HR,GPP,NEE,NPP,SMINN,SMINN_TO_PLANT,DIC_vr,SIC_vr,H2OSOI,H2OSFC,SOILLIQ,SOILICE,ZWT,\
 QFLX_EVAP_TOT,QVEGT,watsat,chem_dt,soil_salinity,soil_pH,DOC_vr,DIC_vr,DOC_RUNOFF,DIC_RUNOFF,SMIN_NO3_RUNOFF,\
-soil_sulfate,soil_sulfide,CH4_vr,CH4FLUX_ALQUIMIA,QDRAI,TSOI,soil_Fe2,soil_FeOxide,soil_FeS,soil_acetate, H2OSFC_TIDE"
+soil_sulfate,soil_sulfide,CH4_vr,CH4FLUX_ALQUIMIA,QDRAI,TSOI,soil_Fe2,soil_FeOxide,soil_FeS,soil_acetate, H2OSFC_TIDE, ALT"
 python site_fullrun.py --site AK-BEO --sitegroup NGEEArctic --caseidprefix Alaska_alquimia_test_BAM  \
 --nyears_ad_spinup 15 --tstep 1 --nyears_final_spinup 15 --nyears_transient 15 \
 --cpl_bypass --machine cades --no_dynroot --gswp3 --nofire --nopftdyn --nopointdata \
