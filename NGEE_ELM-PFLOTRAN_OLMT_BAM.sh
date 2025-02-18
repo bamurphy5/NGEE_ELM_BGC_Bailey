69fb41376ddca541388eb0c3d331ec15940fb62d
Bailey Murphy <42819353+bamurphy5@users.noreply.github.com>
2025-02-18 11:59:49 -0500

    Update NGEE_ELM-PFLOTRAN_OLMT_BAM.sh
    Run option w/extended spinup & using arctic PFTs

--- NGEE_ELM-PFLOTRAN_OLMT_BAM.sh
+++ NGEE_ELM-PFLOTRAN_OLMT_BAM.sh
@@ -133,3 +133,31 @@ python site_fullrun.py --site AK-BEO --sitegroup NGEEArctic --caseidprefix Alask
 --alquimia_ad /home/9v6/ELM-alquimia/REDOX-PFLOTRAN/ELM_decks/CTC_alquimia_forELM_O2consuming_adspinup.in \
 --marsh --tide_forcing_file /nfs/data/ccsi/proj-shared/b0u/NGEE_ELM/BEO_hydro_BC_multicell.nc \
 --parm_file /nfs/data/ccsi/proj-shared/b0u/NGEE_ELM/parms_BEO.txt
+
+
+#2/18/25
+#this version is even longer spinup and AD spinup, includes tidal forcing, uses my updated surface files. 
+#Updated to also output ALT, H2OSFC_TIDE, FCH4, RAIN, TSA, FSAT, ZWT_PERCH
+#to use the arctic PFTs the surfdata file is what needs to be updated, so the version of that w/my udates is also included
+site=beo
+metdir=/nfs/data/ccsi/proj-shared/E3SM/pt-e3sm-inputdata/atm/datm7/GSWP3_daymet/cpl_bypass_$site
+domain=/home/9v6/NGEE_ELM/BEO_domain_multicell.nc
+surf=/home/9v6/NGEE_ELM/BEO_surfdata_multicell_arcticpfts.nc
+varlist="TOTVEGC,TOTSOMC,TOTLITC,SOIL1C_vr,SOIL2C_vr,SOIL3C_vr,SOIL4C_vr,LITR1C_vr,LITR2C_vr,LITR3C_vr,LEAFC,\
+soil_O2,HR,GPP,NEE,NPP,SMINN,SMINN_TO_PLANT,DIC_vr,SIC_vr,H2OSOI,H2OSFC,SOILLIQ,SOILICE,ZWT,\
+QFLX_EVAP_TOT,QVEGT,watsat,chem_dt,soil_salinity,soil_pH,DOC_vr,DIC_vr,DOC_RUNOFF,DIC_RUNOFF,SMIN_NO3_RUNOFF,\
+soil_sulfate,soil_sulfide,CH4_vr,CH4FLUX_ALQUIMIA,QDRAI,TSOI,soil_Fe2,soil_FeOxide,soil_FeS,soil_acetate,H2OSFC_TIDE,ALT,\
+FCH4,RAIN,TSA,FSAT,ZWT_PERCH"
+python site_fullrun.py --site AK-BEO --sitegroup NGEEArctic --caseidprefix Alaska_alquimia_BAM  \
+--nyears_ad_spinup 200 --nyears_final_spinup 500 --tstep 1 --nyears_transient 151 \
+--cpl_bypass --machine cades --no_dynroot --gswp3 --nofire --nopftdyn --nopointdata \
+--model_root /home/9v6/ELM-alquimia/E3SM --ccsm_input /nfs/data/ccsi/proj-shared/E3SM/pt-e3sm-inputdata \
+--domainfile $domain \
+--surffile $surf --np 7 --walltime 24 \
+--caseroot ~/cases --runroot /lustre/or-scratch/cades-ccsi/scratch/9v6/  --mpilib openmpi --pio_version 2 \
+--hist_nhtfrq_trans -1 --hist_mfilt_trans 8760 --hist_mfilt_spinup 0 --hist_nhtfrq_spinup 12 --cn_only \
+--trans_varlist $varlist \
+--alquimia /home/9v6/ELM-alquimia/REDOX-PFLOTRAN/ELM_decks/CTC_alquimia_forELM_O2consuming.in \
+--alquimia_ad /home/9v6/ELM-alquimia/REDOX-PFLOTRAN/ELM_decks/CTC_alquimia_forELM_O2consuming_adspinup.in \
+--marsh --tide_forcing_file /nfs/data/ccsi/proj-shared/b0u/NGEE_ELM/BEO_hydro_BC_multicell.nc \
+--parm_file /nfs/data/ccsi/proj-shared/b0u/NGEE_ELM/parms_BEO.txt
