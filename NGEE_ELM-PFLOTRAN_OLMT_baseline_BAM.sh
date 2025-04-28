6353a0279eee356c687e456d3fb0a336c26c3ca3
Bailey Murphy <42819353+bamurphy5@users.noreply.github.com>
2025-04-28 14:44:50 -0400

    Create NGEE_ELM-PFLOTRAN_OLMT_baseline_BAM.sh
    adapted bash run script for CADES baseline set up

---
+++ NGEE_ELM-PFLOTRAN_OLMT_baseline_BAM.sh
@@ -0,0 +1,28 @@
+#4/28/25
+#version of the job submission script for use w/CADES baseline setup (bails not 9v6)
+#Ben recently updated the master tidal branch to incorporate the surface gas flux changes and to use freshwater instead of saltwater freezing point to resolve issues w/Teri lowering the tidal forcing freezing point that potentially were resulting in the lack of freezing at depth we were seeing
+#this uses the short spin up for testing (200 years instead of 600 years)
+site=beo
+metdir=/gpfs/wolf2/cades/cli185/proj-shared/pt-e3sm-inputdata/atm/datm7/GSWP3_daymet/cpl_bypass_$site
+domain=/home/bails/NGEE_ELM/BEO_domain_multicell.nc
+surf=/home/bails/NGEE_ELM/BEO_surfdata_multicell_arcticpfts_hydro.nc
+paramfile=/home/bails/NGEE_ELM/clm_params_arctic_updated.nc
+varlist="TOTVEGC,TOTSOMC,TOTLITC,SOIL1C_vr,SOIL2C_vr,SOIL3C_vr,SOIL4C_vr,LITR1C_vr,LITR2C_vr,LITR3C_vr,LEAFC,\
+soil_O2,HR,GPP,NEE,NPP,SMINN,SMINN_TO_PLANT,DIC_vr,SIC_vr,H2OSOI,H2OSFC,SOILLIQ,SOILICE,ZWT,\
+QFLX_EVAP_TOT,QVEGT,watsat,chem_dt,soil_salinity,soil_pH,DOC_vr,DIC_vr,DOC_RUNOFF,DIC_RUNOFF,SMIN_NO3_RUNOFF,\
+soil_sulfate,soil_sulfide,CH4_vr,CH4FLUX_ALQUIMIA,QDRAI,TSOI,soil_Fe2,soil_FeOxide,soil_FeS,soil_acetate,H2OSFC_TIDE,ALT,\
+FCH4,RAIN,TSA,FSAT,ZWT_PERCH,TBOT"
+python site_fullrun.py --site AK-BEO --sitegroup NGEEArctic --caseidprefix Alaska_alquimia_arctic_BAM_1 \
+--nyears_ad_spinup 200 --nyears_final_spinup 200 --tstep 1 --nyears_transient 151 \
+--cpl_bypass --machine cades-baseline --no_dynroot --gswp3 --daymet4 --nofire --nopftdyn --nopointdata \
+--model_root /home/bails/ELM-alquimia/E3SM --ccsm_input /gpfs/wolf2/cades/cli185/proj-shared/pt-e3sm-inputdata \
+--domainfile $domain \
+--surffile $surf --np 7 --walltime 48 --maxpatch_pft 12 \
+--mod_parm_file $paramfile \
+--caseroot ~/cases --runroot /gpfs/wolf2/cades/cli185/scratch/bails/  --mpilib openmpi --pio_version 2 \
+--hist_nhtfrq_trans -1 --hist_mfilt_trans 8760 --hist_mfilt_spinup 0 --hist_nhtfrq_spinup 12 --cn_only \
+--trans_varlist $varlist \
+--alquimia /home/bails/ELM-alquimia/REDOX-PFLOTRAN/ELM_decks/CTC_alquimia_forELM_O2consuming.in \
+--alquimia_ad /home/bails/ELM-alquimia/REDOX-PFLOTRAN/ELM_decks/CTC_alquimia_forELM_O2consuming_adspinup.in \
+--marsh --tide_forcing_file /nfs/data/ccsi/proj-shared/b0u/NGEE_ELM/BEO_hydro_BC_multicell.nc \
+--parm_file /home/bails/NGEE_ELM/parms_BEO.txt
