23d87e9d0102b032afc6cf343ac89b12ebd241fa
Bailey Murphy <42819353+bamurphy5@users.noreply.github.com>
2025-06-06 18:10:25 -0400

    Update NGEE_ELM-PFLOTRAN_OLMT_baseline_BAM.sh

--- NGEE_ELM-PFLOTRAN_OLMT_baseline_BAM.sh
+++ NGEE_ELM-PFLOTRAN_OLMT_baseline_BAM.sh
@@ -119,14 +119,14 @@ python site_fullrun.py --site AK-BEO --sitegroup NGEEArctic --caseidprefix Alask
 site=beo
 metdir=/gpfs/wolf2/cades/cli185/proj-shared/pt-e3sm-inputdata/atm/datm7/GSWP3_daymet/cpl_bypass_$site
 domain=$HOME/NGEE_ELM/BEO_domain_multicell.nc
-surf=$HOME/NGEE_ELM/BEO_surfdata_multicell_arcticpfts_hydro_higherfdrain.nc
+surf=$HOME/NGEE_ELM/BEO_surfdata_multicell_arcticpfts_hydro_higherfdrain2.nc
 paramfile=$HOME/NGEE_ELM/clm_params_arctic_updated.nc
 varlist="TOTVEGC,TOTSOMC,TOTLITC,SOIL1C_vr,SOIL2C_vr,SOIL3C_vr,SOIL4C_vr,LITR1C_vr,LITR2C_vr,LITR3C_vr,LEAFC,\
 soil_O2,HR,GPP,NEE,NPP,SMINN,SMINN_TO_PLANT,DIC_vr,SIC_vr,H2OSOI,H2OSFC,SOILLIQ,SOILICE,ZWT,\
 QFLX_EVAP_TOT,QVEGT,watsat,chem_dt,soil_salinity,soil_pH,DOC_vr,DIC_vr,DOC_RUNOFF,DIC_RUNOFF,SMIN_NO3_RUNOFF,\
 soil_sulfate,soil_sulfide,CH4_vr,CH4FLUX_ALQUIMIA,QDRAI,TSOI,soil_Fe2,soil_FeOxide,soil_FeS,soil_acetate,H2OSFC_TIDE,ALT,\
 FCH4,RAIN,TSA,FSAT,ZWT_PERCH,TBOT"
-python site_fullrun.py --site AK-BEO --sitegroup NGEEArctic --caseidprefix Alaska_alquimia_arctic_BAM_8 \
+python site_fullrun.py --site AK-BEO --sitegroup NGEEArctic --caseidprefix Alaska_alquimia_arctic_BAM_9 \
 --nyears_ad_spinup 200 --nyears_final_spinup 200 --tstep 1 --nyears_transient 100 \
 --cpl_bypass --machine cades-baseline --no_dynroot --gswp3 --nofire --nopftdyn --nopointdata \
 --model_root $HOME/ELM-alquimia/E3SM --ccsm_input /gpfs/wolf2/cades/cli185/proj-shared/pt-e3sm-inputdata \
