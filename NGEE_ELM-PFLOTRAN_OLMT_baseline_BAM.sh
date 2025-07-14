60004fa7b09c1fe2890e489407a6401beff459e7
Bailey Murphy <42819353+bamurphy5@users.noreply.github.com>
2025-07-14 15:14:31 -0400

    Update NGEE_ELM-PFLOTRAN_OLMT_baseline_BAM.sh

--- NGEE_ELM-PFLOTRAN_OLMT_baseline_BAM.sh
+++ NGEE_ELM-PFLOTRAN_OLMT_baseline_BAM.sh
@@ -142,17 +142,21 @@ python site_fullrun.py --site AK-BEO --sitegroup NGEEArctic --caseidprefix Alask
 #--parm_file $HOME/NGEE_ELM/parms_BEO_may.txt
 
 #6/27/25: trying running ELM w/o PFLOTRAN turned on to just look at the default CH4 model output
+#7/14/25 added outputs to have the vertically resolved CH4 and O2 for the default model as well, using surface file w/differences by poylgon type set up
+#see notes in "testing_drainage_rates_2.R"
 site=beo
 metdir=/gpfs/wolf2/cades/cli185/proj-shared/pt-e3sm-inputdata/atm/datm7/GSWP3_daymet/cpl_bypass_$site
 domain=$HOME/NGEE_ELM/BEO_domain_multicell.nc
-surf=$HOME/NGEE_ELM/BEO_surfdata_multicell_arcticpfts_hydro_higherfdrain2.nc
+surf=$HOME/NGEE_ELM/BEO_surfdata_multicell_arcticpfts_polygon_diff.nc
 paramfile=$HOME/NGEE_ELM/clm_params_arctic_updated.nc
 varlist="TOTVEGC,TOTSOMC,TOTLITC,SOIL1C_vr,SOIL2C_vr,SOIL3C_vr,SOIL4C_vr,LITR1C_vr,LITR2C_vr,LITR3C_vr,LEAFC,\
 HR,GPP,NEE,NPP,SMINN,SMINN_TO_PLANT,H2OSOI,H2OSFC,SOILLIQ,SOILICE,ZWT,\
 QFLX_EVAP_TOT,QVEGT,watsat,TSOI,H2OSFC_TIDE,ALT,\
-FCH4,FCH4TOCO2,CH4PROD,RAIN,TSA,FSAT,ZWT_PERCH,TBOT"
-python site_fullrun.py --site AK-BEO --sitegroup NGEEArctic --caseidprefix Alaska_defaultCH4_arctic_BAM_1 \
---nyears_ad_spinup 200 --nyears_final_spinup 200 --tstep 1 --nyears_transient 100 \
+FCH4,FCH4TOCO2,CH4PROD,RAIN,TSA,FSAT,ZWT_PERCH,TBOT,SIF,\
+FINUNDATED,CH4_SURF_DIFF_SAT,CH4_SURF_DIFF_UNSAT,CH4_EBUL_TOTAL_SAT,CH4_EBUL_TOTAL_UNSAT,CH4_SURF_EBUL_SAT,\
+CH4_SURF_EBUL_UNSAT,CH4_SURF_AERE_SAT,CH4_SURF_AERE_UNSAT,CONC_CH4_SAT,CONC_CH4_UNSAT,CH4_OXID_DEPTH_SAT,CH4_OXID_DEPTH_UNSAT,CONC_O2_SAT,CONC_O2_UNSAT"
+python site_fullrun.py --site AK-BEO --sitegroup NGEEArctic --caseidprefix Alaska_defaultCH4_arctic_BAM_2 \
+--nyears_ad_spinup 300 --nyears_final_spinup 400 --tstep 1 --nyears_transient 165 \
 --cpl_bypass --machine cades-baseline --no_dynroot --gswp3 --nofire --nopftdyn --nopointdata \
 --model_root $HOME/ELM-alquimia/E3SM --ccsm_input /gpfs/wolf2/cades/cli185/proj-shared/pt-e3sm-inputdata \
 --domainfile $domain \
